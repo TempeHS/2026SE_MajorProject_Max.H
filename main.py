8268a015ba11a84bc595fde18179ad0ea729002e
Max H <143594972+sphisi@users.noreply.github.com>
2026-05-20 02:51:35 +0000

    created: plans, relearnt flask

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import database_manager as dbhandler
+
+app = Flask(__name__)
+
+
+@app.route("/")
+
+
+@app.route("/index")
+def index(): ...
+
+
+@app.route("/search", methods=["GET", "POST"])
